5e589dc3f5c230b739d78028426aa748855e8cbb
Legion112 <ageev.maks2014@gmail.com>
2021-04-08 17:13:17 +0300

    Create bc break

--- src/Events/TestSomethingHappenedEvent.php
+++ src/Events/TestSomethingHappenedEvent.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace HomeappCorporate\RegistryMessages\Events;
 
-class TestSomethingHappenedEvent
+final class TestSomethingHappenedEvent
 {
     public string $description;
+    public int $number;
 
-    public function __construct(string $description)
+    public function __construct(int $number, string $description)
     {
+        $this->number = $number;
         $this->description = $description;
     }
 }
